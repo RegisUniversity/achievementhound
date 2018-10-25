822f70445bcc27499b8ef273d8edd184120ea914
Saikat1239 <mukhopadhyay.saikat@gmail.com>
2018-10-25 14:05:53 -0600

    Update viewreport.php
    Saikat Modified

--- viewreport.php
+++ viewreport.php
@@ -1,34 +1,59 @@
 <?php
-require_once('./LatexTemplate.php');
+// database variables
 
-$test = "";
-if(isset($_GET['t'])) {
-	// Make the LaTeX file and send it through
-	$test = $_GET['t'];
-	if($test =="") {
-		// Test pattern to show symbol handling
-		for($i = 0; $i < 256; $i++) {
-			$test .= chr($i) . " . ";
-		}
-	}
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "achievementhound";
 
-	try {
-		LatexTemplate::download('This is for testing data', 'minimal.tex', 'document.pdf');
-	} catch(Exception $e) {
-		echo $e -> getMessage();
-	}
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
+$sql = "SELECT user_fname, user_lname,user_email,user_phone  FROM tbl_user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $data= '';
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $line = '';
+        
+            foreach( $row as $value )
+            {                                            
+                if ( ( !isset( $value ) ) || ( $value == "" ) )
+                {
+                    $value = "\t";
+                }
+                else
+                {
+                    $value = str_replace( '"' , '""' , $value );
+                    $value = '"' . $value . '"' . "\t";
+                }
+                $line .= $value;
+            }
+            $data .= trim( $line ) . "\n";
+    }
+} else {
+    echo "0 results";
 }
-?>
-<html>
-<head>
-<title>LaTeX test (minimal)</title>
-</head>
-</html>
-<body>
-	<p>Enter some text to be placed on the output:</p>
-	<form>
-		<input type="text" name="t" /><input type="submit" value="Generate" />
-	</form>
-</body>
-</html>
+$data = str_replace( "\r" , "" , $data );
+
+if ( $data == "" )
+{
+    $data = "\n(0) Records Found!\n";                        
+}
+
+header("Content-type: application/octet-stream");
+//header("Content-Disposition: attachment; filename=testReport.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
+$csv_filename="testReport.csv";
+$fd = fopen ($csv_filename, "w");
+fputs($fd, $data);
+fclose($fd);
+
+$conn->close();
